47223051c93968f5c925578cb808be8e00b7b079
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-12-15 01:52:58 +0200

    add logging

--- h2/src/test/org/h2/samples/HelloWorld.java
+++ h2/src/test/org/h2/samples/HelloWorld.java
@@ -10,12 +10,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import org.h2.tools.DeleteDbFiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A very simple class that shows how to load the driver, create a database,
  * create a table, and insert some data.
  */
 public class HelloWorld {
+    private final static Logger log = LoggerFactory.getLogger(HelloWorld.class);
 
     /**
      * Called when ran from command line.
@@ -30,6 +33,7 @@ public class HelloWorld {
         Class.forName("org.h2.Driver");
 //        Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
         Connection conn = DriverManager.getConnection("jdbc:h2:mem:");
+        log.info("=== Connected to database ===\n");
         Statement stat = conn.createStatement();
 
         // this line would initialize the database
@@ -37,12 +41,12 @@ public class HelloWorld {
         // stat.execute("runscript from 'init.sql'");
 
         stat.execute("create table test(id int primary key, name varchar(255))");
-        stat.execute("create table test_2(id int primary key, name varchar(255))");
-        stat.execute("insert into test values(1, 'Hello')");
-        conn.commit();
-        stat.execute("create table test_3(id int primary key, name varchar(255))");
-        stat.execute("insert into test values(2, 'Hello')");
-        conn.commit();
+//        stat.execute("create table test_2(id int primary key, name varchar(255))");
+//        stat.execute("insert into test values(1, 'Hello')");
+//        conn.commit();
+//        stat.execute("create table test_3(id int primary key, name varchar(255))");
+//        stat.execute("insert into test values(2, 'Hello')");
+//        conn.commit();
 
         ResultSet rs;
         rs = stat.executeQuery("select * from test");
