1590fd9e89b8ca69aad9f7ac5b1e40af33cf7e5d
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-12-16 19:41:01 +0200

    add logging

---
+++ h2/src/test/org/h2/learn/MVStoreTest.java
@@ -0,0 +1,20 @@
+package org.h2.learn;
+
+import org.h2.mvstore.MVMap;
+import org.h2.mvstore.MVStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MVStoreTest {
+    private final static Logger log = LoggerFactory.getLogger(MVStoreTest.class);
+
+    public static void main(String[] args) {
+        MVStore s = MVStore.open(null);
+
+        MVMap<Integer, String> map = s.openMap("data");
+        map.put(1, "Hello World");
+        log.info("{}", map.get(1));
+
+        s.close();
+    }
+}
